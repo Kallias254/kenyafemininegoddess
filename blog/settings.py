d50e4ec02ccf1fbe9fba370b87fb65101cdd9fe9
Enock <djsean@Enocks-MacBook-Pro.local>
2024-10-26 01:41:30 +0300

    updated settings

--- blog/settings.py
+++ blog/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,14 +136,12 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-
 ## For media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
--- build.sh
+++ build.sh
@@ -9,5 +9,6 @@ echo "Building the project JDA..."
 
 pip install -r requirements.txt
 
+python manage.py collectstatic --no-input
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
