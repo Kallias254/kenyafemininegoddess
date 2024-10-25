6fbf7b31b9fb1920ceac1be83e553db70af3a6ad
Enock <djsean@Enocks-MacBook-Pro.local>
2024-10-25 20:16:15 +0300

    updated requirements

--- blog/settings.py
+++ blog/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
+import dj_database_url
+from decouple import config
 from pathlib import Path, os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jqx3e+sq2(sja+kuxr6(t5oijbe6(9jaf!1ieat0raf0nb&w=w'
+SECRET_KEY = 'seen'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
